912e89374307610a92083ca8f5be88b3de6b23fd
mswist <mswist@users.noreply.github.com>
2023-02-16 17:28:41 +0100

    import

--- supa.js
+++ supa.js
@@ -1,3 +1,5 @@
+import createClient from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"
+
 SUPABASE_URL = "https://lbozubrlkudonsaefqet.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxib3p1YnJsa3Vkb25zYWVmcWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY1NjI1ODIsImV4cCI6MTk5MjEzODU4Mn0.OagUu0A_m1sG7eeUi_BGKOuNa1nJ6OnrpgdmuNeij_E"
 
