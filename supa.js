ce9ba60668003be55f42f2e21eba0c9f86cbcfb3
mswist <mswist@users.noreply.github.com>
2023-02-16 17:45:27 +0100

    channel fixes

--- supa.js
+++ supa.js
@@ -1,16 +1,18 @@
 const SUPABASE_URL = "https://lbozubrlkudonsaefqet.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxib3p1YnJsa3Vkb25zYWVmcWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY1NjI1ODIsImV4cCI6MTk5MjEzODU4Mn0.OagUu0A_m1sG7eeUi_BGKOuNa1nJ6OnrpgdmuNeij_E"
+const { createClient } = supabase
 
-const supabase_con = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-supabase_con
-  .channel('test', {
-    config: {
-      broadcast: {
-        self: true,
-      },
+const channel = supabase.channel('test', {
+  config: {
+    broadcast: {
+      self: true,
     },
-  })
+  },
+})
+
+channel
   .on('broadcast', { event: 'supa' }, (payload) => console.log(payload))
   .subscribe((status) => {
     if (status === 'SUBSCRIBED') {
