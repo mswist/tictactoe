f529b2596d18320e96bd182dc44bbbe2b04ec34e
mswist <mswist@users.noreply.github.com>
2023-02-16 17:33:59 +0100

    createClient access

--- supa.js
+++ supa.js
@@ -1,11 +1,9 @@
-import createClient from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"
+const SUPABASE_URL = "https://lbozubrlkudonsaefqet.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxib3p1YnJsa3Vkb25zYWVmcWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY1NjI1ODIsImV4cCI6MTk5MjEzODU4Mn0.OagUu0A_m1sG7eeUi_BGKOuNa1nJ6OnrpgdmuNeij_E"
 
-SUPABASE_URL = "https://lbozubrlkudonsaefqet.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxib3p1YnJsa3Vkb25zYWVmcWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY1NjI1ODIsImV4cCI6MTk5MjEzODU4Mn0.OagUu0A_m1sG7eeUi_BGKOuNa1nJ6OnrpgdmuNeij_E"
+const supabase_con = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
-
-supabase
+supabase_con
   .channel('test', {
     config: {
       broadcast: {
