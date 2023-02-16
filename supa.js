12078ad4391cfcdeee80fdd88968516a4c71f5da
mswist <mswist@users.noreply.github.com>
2023-02-16 17:47:24 +0100

    supabase fix

--- supa.js
+++ supa.js
@@ -2,9 +2,9 @@ const SUPABASE_URL = "https://lbozubrlkudonsaefqet.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxib3p1YnJsa3Vkb25zYWVmcWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY1NjI1ODIsImV4cCI6MTk5MjEzODU4Mn0.OagUu0A_m1sG7eeUi_BGKOuNa1nJ6OnrpgdmuNeij_E"
 const { createClient } = supabase
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+const supClient = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-const channel = supabase.channel('test', {
+const channel = supClient.channel('test', {
   config: {
     broadcast: {
       self: true,
