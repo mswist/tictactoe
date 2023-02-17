156c55320ef78bf32b294f1f669e7efa041f4c70
Maciej Swist <swist.maciej@gmail.com>
2023-02-17 22:17:24 +0100

    win

--- tictactoe.js
+++ tictactoe.js
@@ -113,10 +113,12 @@ function checkFive(cCol, cRow, clicked, sign) {
 }
 
 function Win(sign, array) {
-	array.forEach(cell => {
-		board.rows[cell[0]].cells[cell[1]].style.backgroundColor = "rgba(255,0,0,0.3)"
-	})
-	setTimeout(() => {alert(sign+" wins!");	}, 250); 
+	setTimeout(() => {
+		array.forEach(cell => {
+			board.rows[cell[0]].cells[cell[1]].style.backgroundColor = "rgba(255,0,0,0.3)"
+		})
+		alert(sign+" wins!");	
+	}, 300); 
 /* 	var cells = board.getElementsByTagName("td");
 	for(cell in cells) {
 		if(cells.propertyIsEnumerable(cell)) cells[cell].dataset.sign="";
