97e6a375139a45e595e25174a6b40cee356b5f45
mswist <mswist@users.noreply.github.com>
2026-04-03 18:48:49 +0200

    Update Supabase key in supa.js

--- supa.js
+++ supa.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://sfwfyzjzzfpisuzsgnrf.supabase.co"
 //const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmd2Z5emp6emZwaXN1enNnbnJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3Nzg1NjUsImV4cCI6MjA3ODM1NDU2NX0.v5VlZgEiWG45Gl2W_nd5o9C94UY2EUIZiI19loOFgOM"
-const SUPABASE_KEY = "sb_secret_4fMfal3jD1QXVaKhNeN8EA_qUkQr4SF"
+const SUPABASE_KEY = "sb_publishable_ULCO_9jbrg532oo7SNngQg_GYBHdGZk"
 const { createClient } = supabase
 
 const supClient = createClient(SUPABASE_URL, SUPABASE_KEY)
